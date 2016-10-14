f26b386e3f8d96f4c0d018698d8628485a5b8ff3
Dmitrii Zolotov <dzolotov@herzen.spb.ru>
2016-10-14 19:56:27 +0300

    Delete patches.sh

--- patches.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-echo "solr.backup.alfresco.cronExpression=0 0 0 1 1 ? 2200" >> "/opt/alfresco/tomcat/shared/classes/alfresco-global.properties"
-echo "solr.backup.archive.cronExpression=0 0 0 1 1 ? 2200" >>  "/opt/alfresco/tomcat/shared/classes/alfresco-global.properties"
-echo "solr.secureComms=none" >> "/opt/alfresco/tomcat/shared/classes/alfresco-global.properties"
-echo "system.content.orphanProtectDays=1" >>  "/opt/alfresco/tomcat/shared/classes/alfresco-global.properties"
